5c3b6da8cc7ddc52a59c4dd9a7baa2e17e0ddd4e
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-07-24 19:26:05 -0300

    Corrupção no Código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void AdicionarAssinatura()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("FirstName", "lastName", "123456789", "tst@tst.tst");
+        student.AddSubscription(subscription);
+    }
+}
